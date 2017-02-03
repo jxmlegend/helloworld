60dae90a2b4176ca8b3840f5c0d372ed44d5cc27
jxmlegend <jxmlegend@163.com>
2017-02-03 10:12:43 +0800

    java测试程序
    第一次上传java程序

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello
+{
+		public static void main(String args[])
+		{
+			String str="hello world";
+			int a=10;
+			int b=30;
+			int c=a+b;
+			System.out.println(str);	
+			if(a==10)
+			{
+					System.out.println("a==10");
+				}
+			System.out.printf("c===%d",c);	
+		}
+}
